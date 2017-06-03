bc1419992f4931096cebf81b883b42a69260542c
Nico Hauser <me@tyratox.ch>
2017-06-03 18:22:16 +0200

    Added img and css to the build

--- Makefile
+++ Makefile
@@ -1,8 +1,17 @@
-WEBPACK = webpack
+SHELL := /bin/bash
+WEBPACK = node_modules/webpack/bin/webpack.js
 BUILD_CONFIG = ./webpack-build.config.js
 BUILD = ./build
 
 all:
-	$(WEBPACK) --config $(BUILD_CONFIG)
+	if [ -e "config.json" ]; then \
+		rm -rf $($BUILD); \
+		cp web/index.html build/index.html; \
+		cp -r web/css/ build/css/; \
+		cp -r web/img build/img; \
+		$(WEBPACK) --config "$(BUILD_CONFIG)"; \
+	else \
+		echo "config.json doesn't exist!"; \
+	fi
 clean:
-	rm -rf $(BUILD)/
+	rm -rf $(BUILD)
--- webpack-build.config.js
+++ webpack-build.config.js
@@ -1,103 +1,96 @@
-const path		= require('path');
-const webpack	= require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 process.traceDeprecation = true; //https://github.com/webpack/loader-utils/issues/56
 
 module.exports = {
-	entry: [
-		path.join(__dirname, 'web/main.jsx')
-	],
-	
+	entry: [path.join(__dirname, "web/main.jsx")],
+
 	output: {
-		path		: path.join(__dirname, 'build/'),
-		filename	: 'bundle.js',
-		publicPath	: '/build/'
+		path: path.join(__dirname, "build/js/"),
+		filename: "bundle.js",
+		publicPath: "/js/"
 	},
-	
+
 	plugins: [],
-    
-    resolve: {
-	    
-	    modules: [
-		    path.resolve(__dirname),
-		    path.resolve(__dirname, 'node_modules')
-	    ],
-	    
-	    alias: {
-		    core		: path.resolve(__dirname, 'node_modules', 'booki-frontend-core'),
+
+	resolve: {
+		modules: [path.resolve(__dirname), path.resolve(__dirname, "node_modules")],
+
+		alias: {
+			core: path.resolve(__dirname, "node_modules", "booki-frontend-core")
 		},
-		
-		extensions: ['.js', '.jsx', '.css', '.scss']
+
+		extensions: [".js", ".jsx", ".css", ".scss"]
 	},
-	
+
 	module: {
 		rules: [
 			{
-				test	: /\.jsx?$/,
-				exclude	: /node_modules(\/|\\)(?!booki-frontend-core(\/|\\))/,
-				
-				use		: [
+				test: /\.jsx?$/,
+				exclude: /node_modules(\/|\\)(?!booki-frontend-core(\/|\\))/,
+
+				use: [
 					{
-						loader	: 'babel-loader',
-						options	: {
-							presets: ['es2015', 'es2016', 'es2017', 'react'],
-							plugins: ['transform-object-rest-spread']
-						}
-					}
-					
-				]
-			},
-			{
-				test	: /\.css$/,
-				exclude	: /node_modules(\/|\\)(?!booki-frontend-core(\/|\\))/,
-				
-				use	: [
-					{
-						loader: 'style-loader',
+						loader: "babel-loader",
 						options: {
-							sourceMap: true
-						}
-					},
-					{
-						loader: 'css-loader',
-						options: {
-							modules			: true,
-							importLoaders	: true,
-							localIdentName	: '[path]___[name]__[local]___[hash:base64:5]'
+							presets: ["es2015", "es2016", "es2017", "react"],
+							plugins: ["transform-object-rest-spread"]
 						}
 					}
 				]
 			},
 			{
-				test	: /\.scss$/,
-				exclude	: /node_modules(\/|\\)(?!booki-frontend-core(\/|\\))/,
-				
-				use		: [
+				test: /\.css$/,
+				exclude: /node_modules(\/|\\)(?!booki-frontend-core(\/|\\))/,
+
+				use: [
 					{
-						loader: 'style-loader',
+						loader: "style-loader",
 						options: {
 							sourceMap: true
 						}
 					},
 					{
-						loader: 'css-loader',
+						loader: "css-loader",
 						options: {
-							modules			: true,
-							importLoaders	: true,
-							localIdentName	: '[path]___[name]__[local]___[hash:base64:5]'
+							modules: true,
+							importLoaders: true,
+							localIdentName: "[path]___[name]__[local]___[hash:base64:5]"
 						}
-					},
+					}
+				]
+			},
+			{
+				test: /\.scss$/,
+				exclude: /node_modules(\/|\\)(?!booki-frontend-core(\/|\\))/,
+
+				use: [
 					{
-						loader: 'resolve-url-loader'
-					},
-					{
-						loader: 'sass-loader',
+						loader: "style-loader",
 						options: {
 							sourceMap: true
 						}
 					},
+					{
+						loader: "css-loader",
+						options: {
+							modules: true,
+							importLoaders: true,
+							localIdentName: "[path]___[name]__[local]___[hash:base64:5]"
+						}
+					},
+					{
+						loader: "resolve-url-loader"
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: true
+						}
+					}
 				]
 			}
 		]
 	}
-}
+};

f4facb90844a3200d266cc0a65cb3b3345ee15c3
Travis CI <builds@travis-ci.org>
2017-06-03 19:09:05 +0000

    Automated travis build

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -29098,7 +29098,7 @@ var Login = function Login() {
 					_react2.default.createElement(
 						"h1",
 						null,
-						"\uD83D\uDCDA Booki Dashboard"
+						"Booki Dashboard"
 					),
 					_react2.default.createElement(
 						"a",
